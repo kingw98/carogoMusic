56264b280d49609c82c9bd08d174bd6c75b68523
lujaro <lujaro@outlook.es>
2018-12-06 12:04:34 -0300

    actualizacion luciano

--- index.php
+++ index.php
@@ -1,35 +1,4 @@
 <!DOCTYPE html>
-
-<?php
-   include("config.php");
-   session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT id_personal FROM personal WHERE nombre = '$myusername' and clave = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-         
-         header("location: sucursal.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
-      }
-   }
-?>
-
 <html lang="en">
 <head>
 	<title>Login V2</title>
