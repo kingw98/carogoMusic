709d29a0605df4dfa06b99c1c74aa3980e00dfc8
lujaro <lujaro@outlook.es>
2018-12-06 10:04:43 -0300

    actualizacion luciano

--- sucursal.php
+++ sucursal.php
@@ -1,9 +1,14 @@
-
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]><!--> 
+
+<?php
+   include('session.php');
+?>
+
+<html class="no-js"> <!--<![endif]-->
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -147,14 +152,9 @@
 	<script src="js/jquery.countTo.js"></script>
 	
 	
-	
 	<!-- MAIN JS -->
 	<script src="js/main.js"></script>
 
 
-
-	
-
 	</body>
 </html>
-
